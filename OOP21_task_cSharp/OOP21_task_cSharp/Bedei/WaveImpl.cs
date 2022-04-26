447d89d6a708df455a3f21a6f0df0641b31b53b2
andrea <andrea.bedei2@studio.unibo.it>
2022-04-26 20:51:54 +0200

    Add class WaveImpl

---
+++ OOP21_task_cSharp/OOP21_task_cSharp/Bedei/WaveImpl.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+
+namespace OOP21_task_cSharp.Bedei
+{
+    internal class WaveImpl : IWave
+    {
+        private readonly List<IEnemy> _wave;
+        List<IEnemy> IWave.EnemyList => _wave;
+    
+        public WaveImpl(List<IEnemy> wave) => _wave = wave;
+    }
+}

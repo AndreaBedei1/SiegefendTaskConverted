911b8c09b5d0425e91886abe6c9c9f0ac6ae1649
andrea <andrea.bedei2@studio.unibo.it>
2022-04-26 20:42:39 +0200

    Add the EnemyEnum

---
+++ OOP21_task_cSharp/OOP21_task_cSharp/Bedei/EnemyType.cs
@@ -0,0 +1,13 @@
+﻿namespace OOP21_task_cSharp.Bedei
+{
+
+    /// <summary>
+    /// Enumeration that denotes the possible enemy types.
+    /// </summary>
+    public enum EnemyType
+    {
+        TANK,
+        PLANE,
+        HELICOPTER
+    }
+}

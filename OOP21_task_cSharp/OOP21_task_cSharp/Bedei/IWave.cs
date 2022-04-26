37501e00386684f9546762567d52c53c11c74e6c
andrea <andrea.bedei2@studio.unibo.it>
2022-04-26 20:51:42 +0200

    Add class IWave

---
+++ OOP21_task_cSharp/OOP21_task_cSharp/Bedei/IWave.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP21_task_cSharp.Bedei
+{
+    /// <summary>
+    /// Representation of a group of Enemy objects.
+    /// </summary>
+    public interface IWave
+    {
+        /// <summary>
+        /// A list containing all the enemies in the wave.
+        /// </summary>
+        List<IEnemy> EnemyList { get; }
+    }
+}

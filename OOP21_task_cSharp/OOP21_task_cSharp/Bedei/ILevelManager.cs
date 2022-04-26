858152b477e6fb784e6de19619b836a352021bd9
andrea <andrea.bedei2@studio.unibo.it>
2022-04-26 21:00:43 +0200

    Add the interface ILevelManager

---
+++ OOP21_task_cSharp/OOP21_task_cSharp/Bedei/ILevelManager.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections.Generic;
+
+namespace OOP21_task_cSharp.Bedei
+{
+    public interface ILevelManager
+    {
+        /// <summary>
+        /// A list containing all the waves in the level.
+        /// </summary>
+        public List<IWave> Waves { get; }
+
+        /// <summary>
+        /// The total number of Wave in the Level.
+        /// </summary>
+        public int TotalWaves { get; }
+
+        /// <summary>
+        /// The Level currently being played.
+        /// </summary>
+        public ILevel CurrentLevel { get; }
+
+        /// <summary>
+        /// The current Wave that is being played.
+        /// </summary>
+        public IWave CurrentWave { get; }
+
+        /// <summary>
+        /// Checks if there is a next ave in the level.
+        /// </summary>
+        public bool hasNextWave { get; }
+
+        /// <summary>
+        /// riggers the spawning of a new Enemy, using the informations in the current Wave.
+        /// </summary>
+        public IEnemy? CurrentEnemy { get; }
+
+        /// <summary>
+        /// Checks if there is a following Wave in the Level.
+        /// </summary>
+        public bool hasNextEnemy { get; }
+
+        /// <summary>
+        /// Checks if there is a following Wave and sets it as active.
+        /// </summary>
+        public void nextWave();
+    }
+}

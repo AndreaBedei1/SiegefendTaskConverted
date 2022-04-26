484f31ad76d5bb645d51fb3a83a75eae3e0a49b3
andrea <andrea.bedei2@studio.unibo.it>
2022-04-26 20:29:30 +0200

    Creation of the project

---
+++ OOP21_task_cSharp/OOP21_task_cSharp/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace OOP21_task_cSharp
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}

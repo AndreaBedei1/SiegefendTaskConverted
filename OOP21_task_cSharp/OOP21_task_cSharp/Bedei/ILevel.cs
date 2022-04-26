b2a63bdd3f35fb5cff24d89cc32ac7205aeed6f3
andrea <andrea.bedei2@studio.unibo.it>
2022-04-26 20:55:42 +0200

    Add interface ILevel

---
+++ OOP21_task_cSharp/OOP21_task_cSharp/Bedei/ILevel.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+
+namespace OOP21_task_cSharp.Bedei
+{
+    /// <summary>
+    /// Represents a level of play.
+    /// </summary>
+    public interface ILevel
+    {
+        /// <summary>
+        /// Returns a list containing all the waves of enemies in the level.
+        /// </summary>
+        public List<IWave> Waves { get; }
+
+        /// <summary>
+        /// Simple getter for the field that contains the Level ID.
+        /// </summary>
+        public int LevelId { get; }
+
+        /// <summary>
+        /// The number of Wave.
+        /// </summary>
+        public int NumberOfWaves { get; }
+    }
+}

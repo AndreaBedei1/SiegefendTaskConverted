ec8030edecfc49d6da1017899d9f7e43c32a6075
andrea <andrea.bedei2@studio.unibo.it>
2022-04-26 21:03:02 +0200

    Add LevelManager class

---
+++ OOP21_task_cSharp/OOP21_task_cSharp/Bedei/LevelManagerImpl.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP21_task_cSharp.Bedei
+{
+    public class LevelManagerImpl : ILevelManager
+    {
+        private readonly ILevel _level;
+        private readonly IEnumerator<IWave> _waveIter;
+        private IEnumerator<IEnemy> _enemyIter;
+        private IWave? _currentWave = null; 
+
+        public LevelManagerImpl(ILevel level)
+        {
+            _level = level;
+            _waveIter = level.Waves.GetEnumerator();
+            loadWave();
+        }
+
+        private void loadWave()
+        {
+            if (_waveIter.MoveNext())
+            {
+                _currentWave = _waveIter.Current;
+                _enemyIter = _waveIter.Current.EnemyList.GetEnumerator();
+            }
+            else
+                throw new NotImplementedException();
+        }
+
+        public List<IWave> Waves => _level.Waves;
+
+        public int TotalWaves => _level.NumberOfWaves;
+
+        public ILevel CurrentLevel => _level;
+
+        public IWave CurrentWave => _currentWave is null ? throw new Exception() : _currentWave;
+
+        public bool hasNextWave => _waveIter.MoveNext();
+
+        public IEnemy? CurrentEnemy => _enemyIter.Current;
+
+        public bool hasNextEnemy => _enemyIter.MoveNext();
+
+        public void nextWave() => loadWave();
+    }
+}

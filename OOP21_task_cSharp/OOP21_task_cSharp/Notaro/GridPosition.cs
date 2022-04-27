9bdea685c049bc3e988693735a0ef96bb16ed319
Fabio <fabio.notaro2@studio.unibo.it>
2022-04-28 01:42:20 +0200

    Create personal folder and add classes PositionConverter and GridPosition

---
+++ OOP21_task_cSharp/OOP21_task_cSharp/Notaro/GridPosition.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace OOP21_task_cSharp.Notaro
+{
+    /// <summary>
+    /// Represents a pair of integer coordinates on a grid (row and column).
+    /// </summary>
+    public class GridPosition
+    {
+        public int Row { get; set; }
+        public int Column { get; set; }
+
+        /// <summary>
+        /// Basic constructor.
+        /// </summary>
+        /// <param name="row">The coordinate as row in the grid.</param>
+        /// <param name="column">The coordinate as column in the grid.</param>
+        public GridPosition(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        /// <summary>
+        /// Sets new values for both the column and row of the position.
+        /// </summary>
+        /// <param name="row">The new value for the row.</param>
+        /// <param name="column">The new value for the column.</param>
+        public void SetCoordinates(int row, int column)
+        {
+            Row = row;
+            Column = column;
+        }
+
+        // Standard methods to compare grid positions.
+        public override bool Equals(object obj)
+        {
+            if (this == obj) 
+            {
+                return true;
+            }
+            if (obj == null)
+            {
+                return false;
+            }
+            return obj is GridPosition position &&
+                   Row == position.Row &&
+                   Column == position.Column;
+        }
+        public override int GetHashCode() => HashCode.Combine(Row, Column);
+        public override string ToString() => @"GridPosition [column = {Column}, row = {Row}]";
+    }
+}
---
+++ OOP21_task_cSharp/OOP21_task_cSharp/Notaro/PositionConverter.cs
@@ -0,0 +1,40 @@
+﻿using OOP21_task_cSharp.Bedei;
+
+namespace OOP21_task_cSharp.Notaro
+{
+    /// <summary>
+    /// Class for converting from Position to GridPosition and vice versa.
+    /// </summary>
+    public class PositionConverter
+    {
+        public int TileSize { get; }    // Property fundamental for the conversion process.
+
+        /// <summary>
+        /// Creates a new instance of the class with a certain Tile size, attribute useful for the conversion.
+        /// </summary>
+        /// <param name="tileSize">Is the size of a tile on the Map.</param>
+        public PositionConverter(int tileSize)
+        {
+            TileSize = tileSize;
+        }
+
+        /// <summary>
+        /// Converts a GridPosition to a Position.
+        /// </summary>
+        /// <param name="gridPosition">The GridPosition to convert.</param>
+        /// <returns>the corresponding Position.</returns>
+        public Position ConvertToPosition(GridPosition gridPosition)
+        {
+            return new Position(gridPosition.Column * TileSize, gridPosition.Row * TileSize);
+        }
+        /// <summary>
+        /// Converts a Position to a GridPosition.
+        /// </summary>
+        /// <param name="position">The Position to convert.</param>
+        /// <returns>the corresponding GridPosition.</returns>
+        public GridPosition ConvertToGridPosition(Position position)
+        {
+            return new GridPosition((int)(position.Y / TileSize), (int)(position.X / TileSize));
+        }
+    }
+}

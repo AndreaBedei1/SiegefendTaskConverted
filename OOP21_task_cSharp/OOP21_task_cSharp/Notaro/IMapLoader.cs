3a0b7de844fc67dd617757043922743bfc8d1fe2
Fabio <fabio.notaro2@studio.unibo.it>
2022-04-29 17:10:22 +0200

    Create interface IMapLoader

---
+++ OOP21_task_cSharp/OOP21_task_cSharp/Notaro/IMapLoader.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP21_task_cSharp.Notaro
+{
+    public interface IMapLoader
+    {
+        public IMap Map { get; }
+    }
+}

e1ee6444fd7ace8e4e6d81f46c11e63f93f7ac9f
Lorenzo <lorenzo.gessi2@gmail.com>
2022-05-03 20:08:15 +0200

    Add class ImgTileSize

---
+++ OOP21_task_cSharp/OOP21_task_cSharp/Gessi/ImgTileSize.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP21_task_cSharp.Gessi
+{
+    /// <summary>
+    /// Disclaimer: I didn't originally write this interface, but now I'm going to write it because I need it and I'm going to do it by simply transcribing the code from Java to C#.
+    /// Class that has the dimension of a single images (tile image).
+    /// </summary>
+    public class ImgTileSize
+    {
+        private const int CellSize = 80;
+        private ImgTileSize() { }
+
+        /// <summary>
+        /// The image size.
+        /// </summary>
+        /// <returns>image size.</returns>
+        public static int GetTileSize()
+        {
+            return CellSize;
+        }
+    }
+}

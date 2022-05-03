6e9956d77f9b0cae2aed65d1ffe6109e3e2c8386
Lorenzo <lorenzo.gessi2@gmail.com>
2022-05-03 20:07:45 +0200

    Add classes Bullet and BulletFactory

---
+++ OOP21_task_cSharp/OOP21_task_cSharp/Gessi/Bullet.cs
@@ -0,0 +1,50 @@
+﻿using OOP21_task_cSharp.Bedei;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP21_task_cSharp.Gessi
+{
+    /// <summary>
+    /// Implementation of a bullet that follows a target.
+    /// </summary>
+    public class Bullet : IBullet
+    {
+        private int _id;
+        private double _speed;
+        private double _damage;
+        private Position _position;
+        private IEnemy _target;
+
+        public Bullet(int id, double speed, Position position, double damage, IEnemy target)
+        {
+            ID = id;
+            Speed = speed;
+            Position = new Position(position);
+            Damage = damage;
+
+
+            if(target == null)
+            {
+                throw new ArgumentNullException("target");
+            }
+            else
+            {
+                Target = target;
+            }
+        }
+
+        public int ID { get => _id; set => _id = value; }
+        public double Speed { get => _speed; set => _speed = value; }
+        public Position Position { get => _position; set => _position = value; }
+        public IEnemy Target { get => _target; set => _target = value; }
+        public Position TargetPosition { get => Target.Position; }
+        public double Damage { get => _damage; set => _damage = value; }
+        public void Move(double x, double y)
+        {
+            Position.SetCoordinates(x, y);
+        }
+    }
+}
---
+++ OOP21_task_cSharp/OOP21_task_cSharp/Gessi/BulletFactory.cs
@@ -0,0 +1,17 @@
+﻿using OOP21_task_cSharp.Bedei;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OOP21_task_cSharp.Gessi
+{
+    public class BulletFactory : IBulletFactory
+    {
+        public IBullet CreateBullet(int id, double speed, Position startPosition, double damage, IEnemy enemyTarget)
+        {
+            return new Bullet(id, speed, startPosition, damage, enemyTarget);
+        }
+    }
+}

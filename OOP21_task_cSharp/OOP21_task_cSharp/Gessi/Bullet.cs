22489cfad867c1e730ea5e3df3bdf7703a17d1ea
Lorenzo <lorenzo.gessi2@gmail.com>
2022-05-03 23:43:05 +0200

    Add new comments, Remove comment, Remove useless usings

--- OOP21_task_cSharp/OOP21_task_cSharp/Gessi/Bullet.cs
+++ OOP21_task_cSharp/OOP21_task_cSharp/Gessi/Bullet.cs
@@ -1,9 +1,5 @@
 ﻿using OOP21_task_cSharp.Bedei;
 using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OOP21_task_cSharp.Gessi
 {
--- OOP21_task_cSharp/OOP21_task_cSharp/Gessi/BulletFactory.cs
+++ OOP21_task_cSharp/OOP21_task_cSharp/Gessi/BulletFactory.cs
@@ -1,14 +1,21 @@
 ﻿using OOP21_task_cSharp.Bedei;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace OOP21_task_cSharp.Gessi
 {
+    /// <summary>
+    /// Creates instances of the <see cref="IBullet"/> interface.
+    /// </summary>
     public class BulletFactory : IBulletFactory
     {
+        /// <summary>
+        /// Creates a new <see cref="IBullet"/> instance with an initial position, target and other parameters.
+        /// </summary>
+        /// <param name="id">the id of the bullet (normally the id of the turret which fires it)</param>
+        /// <param name="speed">the speed of the bullet</param>
+        /// <param name="startPosition">the initial position of the bullet</param>
+        /// <param name="damage">the damage of the bullet</param>
+        /// <param name="enemyTarget">the target of the bullet</param>
+        /// <returns>an <see cref="IBullet"/> instance</returns>
         public IBullet CreateBullet(int id, double speed, Position startPosition, double damage, IEnemy enemyTarget)
         {
             return new Bullet(id, speed, startPosition, damage, enemyTarget);

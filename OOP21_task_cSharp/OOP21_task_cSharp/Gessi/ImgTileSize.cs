973b460d4713768b38a95e3102462daa82578ba4
Lorenzo <lorenzo.gessi2@gmail.com>
2022-05-03 23:44:37 +0200

    Fix comment

--- OOP21_task_cSharp/OOP21_task_cSharp/Gessi/ImgTileSize.cs
+++ OOP21_task_cSharp/OOP21_task_cSharp/Gessi/ImgTileSize.cs
@@ -7,7 +7,7 @@ using System.Threading.Tasks;
 namespace OOP21_task_cSharp.Gessi
 {
     /// <summary>
-    /// Disclaimer: I didn't originally write this interface, but now I'm going to write it because I need it and I'm going to do it by simply transcribing the code from Java to C#.
+    /// Disclaimer: I didn't originally write this class, but now I'm going to write it because I need it and I'm going to do it by simply transcribing the code from Java to C#.
     /// Class that has the dimension of a single images (tile image).
     /// </summary>
     public class ImgTileSize
@@ -18,7 +18,7 @@ namespace OOP21_task_cSharp.Gessi
         /// <summary>
         /// The image size.
         /// </summary>
-        /// <returns>image size.</returns>
+        /// <returns>image size</returns>
         public static int GetTileSize()
         {
             return CELL_SIZE;

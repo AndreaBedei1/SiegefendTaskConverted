43658b4951d29365eaef230aacc7ba6431278759
Lorenzo <lorenzo.gessi2@gmail.com>
2022-05-04 14:03:21 +0200

    Update comments

--- OOP21_task_cSharp/OOP21_task_cSharp/Gessi/IPlayer.cs
+++ OOP21_task_cSharp/OOP21_task_cSharp/Gessi/IPlayer.cs
@@ -1,7 +1,8 @@
 ﻿namespace OOP21_task_cSharp.Gessi
 {
     /// <summary>
-    /// Disclaimer: I didn't originally write this interface, but now I'm going to write it because I need it and I'm going to do it by simply transcribing the code from Java to C#.
+    /// Disclaimer:
+    /// I didn't originally write this class, but now I'm going to write it because I need it and I'm going to do it by simply transcribing (and adapting) the code from Java to C#.
     /// </summary>
     public interface IPlayer
     {
--- OOP21_task_cSharp/OOP21_task_cSharp/Gessi/ImgTileSize.cs
+++ OOP21_task_cSharp/OOP21_task_cSharp/Gessi/ImgTileSize.cs
@@ -1,7 +1,8 @@
 ﻿namespace OOP21_task_cSharp.Gessi
 {
     /// <summary>
-    /// Disclaimer: I didn't originally write this class, but now I'm going to write it because I need it and I'm going to do it by simply transcribing the code from Java to C#.
+    /// Disclaimer:
+    /// I didn't originally write this class, but now I'm going to write it because I need it and I'm going to do it by simply transcribing (and adapting) the code from Java to C#.
     /// Class that has the dimension of a single images (tile image).
     /// </summary>
     public class ImgTileSize

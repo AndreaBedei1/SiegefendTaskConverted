87bb2a873b866380619b8668c813c88f14469e56
Lorenzo <lorenzo.gessi2@gmail.com>
2022-05-05 14:51:40 +0200

    Update message for ArgumentNullException

--- OOP21_task_cSharp/OOP21_task_cSharp/Gessi/Bullet.cs
+++ OOP21_task_cSharp/OOP21_task_cSharp/Gessi/Bullet.cs
@@ -26,7 +26,7 @@ namespace OOP21_task_cSharp.Gessi
 
             if(target == null)
             {
-                throw new ArgumentNullException("target");
+                throw new ArgumentNullException("Target must not be null!");
             }
             else
             {
